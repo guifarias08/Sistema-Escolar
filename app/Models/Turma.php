7848bcd6290cafaadf2da82301a77d99d9d46cb7
Guilherme Pinho <guilhermepinhofariasdeaquino@gmail.com>
2026-08-12 15:15:03 -0300

    remove comentários desnecessários no AlunoController

--- app/Models/Turma.php
+++ app/Models/Turma.php
@@ -9,12 +9,8 @@ class Turma extends Model
 {
     use HasFactory;
 
-    // Autoriza o Laravel a salvar nome e turno no banco
-    protected $fillable = ['nome', 'turno'];
-
-    // Relacionamento com Alunos
-    public function alunos()
-    {
-        return $this->hasMany(Aluno::class);
-    }
+    protected $fillable = [
+        'nome',
+        'serie',
+    ];
 }
